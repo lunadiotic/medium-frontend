6880f80e744357d59cb8e44f33857deafbdca948
IDStack <idstack@outlook.co.id>
2022-02-19 10:39:34 +0700

    post - show main image, title and author

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -15,6 +15,24 @@ const Post = ({post}: Props) => {
   return (
     <main className="max-w-7xl mx-auto">
         <Header />
+
+        <img
+            className='w-full h-40 object-cover'
+            src={urlFor(post.mainImage).url()!} 
+            alt="" 
+        />
+
+        <article className='max-w-3xl mx-auto p-5'>
+            <h1 className='text-3xl mt-10 mb-3'>{post.title}</h1>
+            <h2 className='text-xl font-light text-gray-500 mb-2'>{post.description}</h2>
+
+            <div className='flex items-center space-x-2'>
+                <img className='h-10 w-10 rounded-full' src={urlFor(post.author.image).url()!} alt="" />
+                <p className='font-extralight text-sm'>
+                    Blog post by <span className='text-green-500'>{post.author.name}</span> - Published at {new Date(post.publishedAt).toLocaleString()}
+                </p>
+            </div>
+        </article>
     </main>
   )
 }
